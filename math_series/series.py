6314fd95b7efc900df62df5d484d384e3eafc82f
Iris Leal <iris_leal_m@hotmail.com>
2020-05-12 14:35:41 -0700

    Lucas test and function ready

--- math_series/series.py
+++ math_series/series.py
@@ -1,7 +1,5 @@
 def fibonacci(n):
-    """
-    Returns the value of n in the fibonacci series.
-    """
+    """Returns the integer value of n position in the fibonacci sequence."""
     left = 0
     right = 1
     i = 1
@@ -15,3 +13,18 @@ def fibonacci(n):
     return sum_serie
 
 
+def lucas(n):
+    """Returns the integer value of n position in the lucas sequence."""
+    left = 2
+    right = 1
+    i = 1
+    sum_serie = 0
+    while i <= (n-1):
+        sum_serie = left + right
+        left = right
+        right = sum_serie
+        i+=1
+
+    return sum_serie
+
+print(lucas(7))
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,7 +1,7 @@
 from math_series import __version__
 
-from math_series.series import fibonacci
-# from math_series.series import fibonacci, lucas
+# from math_series.series import fibonacci
+from math_series.series import fibonacci, lucas
 
 def test_version():
     assert __version__ == '0.1.0'
@@ -30,4 +30,24 @@ def test_fibo_four():
 def test_fibo_four():
     actual = fibonacci(14)
     expected = 377
+    assert actual == expected
+
+def test_lucas_one():
+    actual = lucas(4)
+    expected = 7
+    assert actual == expected
+
+def test_lucas_two():
+    actual = lucas(10)
+    expected = 123
+    assert actual == expected
+
+def test_lucas_three():
+    actual = lucas(12)
+    expected = 322
+    assert actual == expected
+
+def test_lucas_four():
+    actual = lucas(7)
+    expected = 29
     assert actual == expected
